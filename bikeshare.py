de8b710e4d4096461ffda474aa19c32e2801ab8d
mariia.bodnar <mariia.bodnar@DEBER03618.local>
2019-09-16 17:59:02 +0200

    removed /n again

--- bikeshare.py
+++ bikeshare.py
@@ -115,7 +115,7 @@ def station_stats(df):
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
-    print('\nCalculating Trip Duration...\n')
+    print('Calculating Trip Duration...\n')
     start_time = time.time()
 
     # TO DO: display total travel time
